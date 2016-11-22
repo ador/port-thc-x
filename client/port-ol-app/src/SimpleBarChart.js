9f3517280ce0dc368a7f0ac3e816039c0c967f34
Adrienn Szabo <aszabo@ilab.sztaki.hu>
2016-11-22 12:19:11 +0100

    Outliers shown in different color

--- client/port-ol-app/src/SimpleBarChart.js
+++ client/port-ol-app/src/SimpleBarChart.js
@@ -3,26 +3,25 @@ import React, { Component } from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 const data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+      {label: '10.0 - 16.2 ', outliers: 9, normal: 0},
+      {label: '16.2 - 22.4 ', outliers: 5, normal: 5},
+      {label: '22.4 - 28.6 ', outliers: 0, normal: 20},
+      {label: '28.6 - 34.8 ', outliers: 0, normal: 50},
+      {label: '34.8 - 43.0 ', outliers: 0, normal: 50}
 ];
+
 class SimpleBarChart extends Component {
   render () {
     return (
       <BarChart width={600} height={300} data={data}
             margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-       <XAxis dataKey="name"/>
+       <XAxis dataKey="label"/>
        <YAxis/>
        <CartesianGrid strokeDasharray="3 3"/>
        <Tooltip/>
        <Legend />
-       <Bar dataKey="pv" fill="#8884d8" />
-       <Bar dataKey="uv" fill="#82ca9d" />
+       <Bar dataKey="outliers" stackId="all" fill="#1BBBBB" />
+       <Bar dataKey="normal" stackId="all" fill="#BBBBBB" />
       </BarChart>
     );
   }
