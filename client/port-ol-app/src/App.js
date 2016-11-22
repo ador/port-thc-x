1f6f5897013497b4cbd4c3b2596e0b4251de2ea4
Adrienn Szabo <aszabo@ilab.sztaki.hu>
2016-11-22 10:42:36 +0100

    A demo chart is added

--- client/port-ol-app/src/App.js
+++ client/port-ol-app/src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SimpleBarChart from './SimpleBarChart.js';
+
 
 class App extends Component {
+  //propTypes: {
+  //  data: React.PropTypes.array.isRequired
+  //},
+
   render() {
     return (
       <div className="App">
@@ -11,8 +17,11 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          Hello, world
+         </p>
+         <SimpleBarChart />
+
+        
       </div>
     );
   }
