f365cb080b1fdb8fbae8185e5777f2ec161d0892
Adrienn Szabo <aszabo@ilab.sztaki.hu>
2016-11-23 15:11:00 +0100

    rename outliers to outlier in client

--- client/port-ol-app/src/SimpleBarChart.js
+++ client/port-ol-app/src/SimpleBarChart.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 const data = [
-      {label: '10.0 - 16.2 ', outliers: 9, normal: 0},
-      {label: '16.2 - 22.4 ', outliers: 5, normal: 5},
-      {label: '22.4 - 28.6 ', outliers: 0, normal: 20},
-      {label: '28.6 - 34.8 ', outliers: 0, normal: 50},
-      {label: '34.8 - 43.0 ', outliers: 0, normal: 50}
+      {label: '10.0 - 16.2 ', outlier: 9, normal: 0},
+      {label: '16.2 - 22.4 ', outlier: 5, normal: 5},
+      {label: '22.4 - 28.6 ', outlier: 0, normal: 20},
+      {label: '28.6 - 34.8 ', outlier: 0, normal: 50},
+      {label: '34.8 - 43.0 ', outlier: 0, normal: 50}
 ];
 
 class SimpleBarChart extends Component {
@@ -20,7 +20,7 @@ class SimpleBarChart extends Component {
        <CartesianGrid strokeDasharray="3 3"/>
        <Tooltip/>
        <Legend />
-       <Bar dataKey="outliers" stackId="all" fill="#1BBBBB" />
+       <Bar dataKey="outlier" stackId="all" fill="#1BBBBB" />
        <Bar dataKey="normal" stackId="all" fill="#BBBBBB" />
       </BarChart>
     );
