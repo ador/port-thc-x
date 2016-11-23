50f760a6e8868662dcf23d95a0c709e90d002a9f
Adrienn Szabo <aszabo@ilab.sztaki.hu>
2016-11-23 11:00:34 +0100

    Python code to label data (WIP)

--- python/portdata/portdata.py
+++ python/portdata/portdata.py
@@ -1,42 +1,54 @@
 import json
 
-class PortData (object):
 
+class PortData (object):
 
     def __init__(self):
         self.settings = dict()
         self.countrycodes = set([])
-        self.last_rates_timestamp = 0 # todo
-        self.data = dict()
-        self.data_by_country = dict() # todo
-
+        self.last_rates_timestamp = 0  # todo
+        self.orig_data = dict()
+        self.data_by_country = dict()
+        self.labeled_data_by_country = dict()
 
     def apply_settings(self, settingsfile):
         with open(settingsfile, 'r') as f:
             self.settings = json.loads(f.read())
-            self.read_data(self.settings["datafile"])
+            self.read_data(self.settings['datafile'])
             f.close()
-
 
     def read_data(self, filename):
         with open(filename, 'r') as f:
-            self.data = json.loads(f.read())
-            for elem in self.data:
-                ccode = elem["port"][0:2]
+            self.orig_data = json.loads(f.read())
+            for elem in self.orig_data:
+                ccode = elem['port'][0:2]
                 if ccode in self.countrycodes:
-                    pass
+                    self.data_by_country[ccode].append(elem)
                 else:
                     self.countrycodes.add(ccode)
-
-            print("all codes found: " + str(self.countrycodes))
+                    self.data_by_country[ccode] = [elem]
             f.close()
         return self.countrycodes
-
 
     def get_countrycodes(self):
         return sorted(list(self.countrycodes))
 
-# todo
-    # def get_all_data_for_country(self, countrycode):
-    #     print("looking for data file in " + str(self.data["datafile"]))
-    #     return self.data[]
+    def get_orig_data_for_country(self, countrycode):
+        return self.data_by_country[countrycode]
+
+    def get_labeled_data_for_country(self, countrycode):
+        if countrycode in self.labeled_data_by_country:
+            return self.labeled_data_by_country[countrycode]
+        else:
+            self.label_data(countrycode)
+            return self.labeled_data_by_country[countrycode]
+
+    def label_data(self, countrycode):
+        datalist = self.data_by_country[countrycode]
+        # reset
+        self.labeled_data_by_country[countrycode] = []
+        # TODO : call outlier detector code here and use labels accordingly
+        for d in datalist:
+            d['label'] = 'OK'
+            self.labeled_data_by_country[countrycode].append(d)
+
--- python/portdata/test_portdata.py
+++ python/portdata/test_portdata.py
@@ -1,20 +1,35 @@
 from portdata.portdata import PortData
-import os 
+import os
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
+# get the path of this script, to use relative paths from here
+this_dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
-def test_read_sample_data():
-    print("dirpath: " + dir_path)
+def setupPortData():
     poda = PortData()
-    poda.apply_settings(dir_path + "/../test_settings.json")
-    data = poda.get_countrycodes()
-    assert(3 == len(data))
-    assert(['CN', 'HU', 'US'] == data)
+    poda.apply_settings(this_dir_path + "/../test_settings.json")
+    return poda
 
 
-# TODO
-# def test_get_histogram():
-#     poda.init()
-#     histogram_with_outliers = poda.get_data_for_country("CN")
-#     assert(10 == len(histogram_with_outliers))
+def test_get_countrycodes():
+    poda = setupPortData()
+    countrycodes = poda.get_countrycodes()
+    assert(3 == len(countrycodes))
+    assert(['CN', 'HU', 'US'] == countrycodes)
+
+
+def test_get_labeled_data_for_country_OK():
+    poda = setupPortData()
+    labeled = poda.get_labeled_data_for_country("CN")
+    assert(10 == len(labeled))
+    assert('CNY' == labeled[0]['currency'])
+    assert('OK' == labeled[0]['label'])
+
+
+# def test_get_labeled_data_for_country_OUTLIER():
+    # TODO 
+    # test data is prepared such that 'supplier_id' is 99 for outliers
+    # assert('OUTLIER' == labeled[x]['label'])
+
+
+# TODO later
