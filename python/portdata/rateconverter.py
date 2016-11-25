671a7a40d0e6426d8a67652273fe76930a74fc10
Adrienn Szabo <aszabo@ilab.sztaki.hu>
2016-11-25 13:22:18 +0100

    RateConverter converts rates :)

--- python/portdata/rateconverter.py
+++ python/portdata/rateconverter.py
@@ -3,21 +3,28 @@ import numpy as np
 
 
 class RateConverter (object):
+    """ Converts values from available currencies to USD.
+        The returned values will be rounded to N decimal points (default: N=2).
+        A configuration settings file can define N, among other parameters.
+    """
+
 
     def __init__(self):
         self.settings = dict()
         self.currencies = []
         self.rates = dict()
+        self.precision = 2
 
     def apply_settings(self, settingsfile):
         with open(settingsfile, 'r') as f:
             self.settings = json.loads(f.read())
             self.read_currencies(self.settings['common_path'] +
                                  self.settings['currency_list_file'])
+            if "currency_conversion_precision" in self.settings:
+                self.precision = self.settings["currency_conversion_precision"]
             f.close()
 
     def read_currencies(self, filename):
-        curr_list = []
         with open(filename, 'r') as f:
             currencies_json = json.loads(f.read())
             self.rates = currencies_json['rates']
@@ -30,9 +37,13 @@ class RateConverter (object):
         return self.currencies
 
     def refresh_rate_values(self):
+        # TODO
         pass
 
     def convert_to_usd(self, from_currency, value):
-        return 100.0
+        if from_currency == "USD":
+            return round(value, self.precision)
+        else:
+            return round(1.0 * value / self.rates[from_currency], self.precision)
 
  
--- python/portdata/test_rateconverter.py
+++ python/portdata/test_rateconverter.py
@@ -17,3 +17,25 @@ def test_available_currencies():
     assert(4 == len(currencies))
     assert(['CNY', 'HUF', 'NOK', 'USD'] == currencies)
 
+
+def test_convert_USD():
+    # Note: precision is set to 1 in the settnigs file 
+    conv = setup_converter_1()
+    assert(1.0 == conv.convert_to_usd("USD", 1.0))
+    assert(14.0 == conv.convert_to_usd("USD", 13.99))
+    assert(0 == conv.convert_to_usd("USD", 0))
+
+
+def test_convert_other():
+    # Note:  rates from the default settings file:
+    # "NOK": 8.578353
+    # "CNY": 6.919257
+    # "HUF": 292.810601
+    conv = setup_converter_1()
+    assert(0.0 == conv.convert_to_usd("HUF", 1))
+    assert(1 == conv.convert_to_usd("HUF", 292.8))
+    assert(34.2 == conv.convert_to_usd("HUF", 10000.0))
+    assert(10 == conv.convert_to_usd("NOK", 85.78))
+    assert(116.6 == conv.convert_to_usd("NOK", 1000))
+    assert(100 == conv.convert_to_usd("CNY", 691.9))
+
